371bb6697dda1fc7c805a7ea1856822761ad91da
Braden-H12345 <68672794+Braden-H12345@users.noreply.github.com>
2021-03-07 14:40:43 -0600

    Added filename property to the class

--- scenefile.py
+++ scenefile.py
@@ -6,6 +6,14 @@ class SceneFile(object):
         self.version = version
         self.ext = ext
 
+    @property
+    def filename(self):
+        pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
+        return pattern.format(descriptor=self.descriptor,
+                              task=self.task,
+                              ver=self.version,
+                              ext=self.ext)
 
-scene_file = SceneFile("C:\\", "tank", "model", "v001", ".ma")
-print(scene_file.descriptor)
+
+scene_file = SceneFile("C:\\", "tank", "model", 1, ".ma")
+print(scene_file.filename)
